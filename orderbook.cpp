cee43b92fd477de002582bffdc0f6c47d9d12477
Pang Rui Wei <>
2024-09-22 17:19:35 +0800

    update orderbook, give user prompt

--- orderbook.cpp
+++ orderbook.cpp
@@ -1,9 +1,11 @@
 #include <curl/curl.h>
 
+#include <chrono>
 #include <iostream>
+#include <map>
+#include <thread>
 #include <websocketpp/client.hpp>
 #include <websocketpp/config/asio_client.hpp>
-#include <map>
 
 #include "json.hpp"
 
@@ -16,27 +18,36 @@ using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using json = nlohmann::json;
 
-std::map<double, double> bids;
+const int ORDER_BOOK_SIZE = 5;
+const std::string GREEN = "\033[32m";
+const std::string RED = "\033[31m";
+const std::string RESET = "\033[0m";
+std::string BINANCE_SNAPSHOT_URL;
+
+std::map<double, double, std::greater<double>> bids;
 std::map<double, double> asks;
 long lastUpdateId;
 
-// update json book
-void updateOrderBook(json jsonSnapShot) {
-    // reset boook
-    bids.clear();
-    asks.clear();
+// print functions
+void printBids() {
+  std::cout << "Bids:" << std::endl;
+  for (auto& p : bids) {
+    std::cout << GREEN << "Price: " << p.first << " Quantity: " << p.second
+              << RESET << std::endl;
+  }
+}
 
-    for (auto& bid : jsonSnapShot["bids"]) {
-        double price = std::stod(bid[0].get<std::string>());
-        double quantity = std::stod(bid[1].get<std::string>());
-        bids[price] = quantity;
-    }
-    
-    for (auto& ask : jsonSnapShot["asks"]) {
-        double price = std::stod(ask[0].get<std::string>());
-        double quantity = std::stod(ask[1].get<std::string>());
-        asks[price] = quantity;
-    }
+void printAsks() {
+  std::cout << "Asks:" << std::endl;
+  for (auto& p : asks) {
+    std::cout << RED << "Price: " << p.first << " Quantity: " << p.second
+              << RESET << std::endl;
+  }
+}
+
+void printOrderBookTitle() {
+  std::cout << "*********ORDERBOOK********************************"
+            << std::endl;
 }
 
 size_t WriteCallback(void* contents, size_t size, size_t nmemb,
@@ -55,15 +66,117 @@ std::string fetchSnapshot() {
   curl = curl_easy_init();
   // setup curl
   if (curl) {
-    curl_easy_setopt(
-        curl, CURLOPT_URL,
-        "https://api.binance.com/api/v3/depth?symbol=BNBBTC&limit=1000");
+    curl_easy_setopt(curl, CURLOPT_URL, BINANCE_SNAPSHOT_URL.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
   }
   return read_buffer;
+}
+
+// update json book
+bool initOrderBook() {
+  // get initial snapshot from binance
+  std::string snapshot = fetchSnapshot();
+  json jsonSnapshot = json::parse(snapshot);
+  // if there is an error from the HTTP request
+  if (jsonSnapshot.find("code") != jsonSnapshot.end()) {
+    std::cerr << "Check your crypto pair" << std::endl;
+    return false;
+  }
+
+  // reset boook
+  asks.clear();
+  bids.clear();
+
+  lastUpdateId = jsonSnapshot["lastUpdateId"];
+  for (auto& bid : jsonSnapshot["bids"]) {
+    double price = std::stod(bid[0].get<std::string>());
+    double quantity = std::stod(bid[1].get<std::string>());
+    bids[price] = quantity;
+  }
+
+  for (auto& ask : jsonSnapshot["asks"]) {
+    double price = std::stod(ask[0].get<std::string>());
+    double quantity = std::stod(ask[1].get<std::string>());
+    asks[price] = quantity;
+  }
+  printOrderBookTitle();
+  printBids();
+  printAsks();
+  return true;
+}
+
+void manageOrderBook(std::string side, json update) {
+  double price = std::stod(update[0].get<std::string>());
+  double quantity = std::stod(update[1].get<std::string>());
+
+  if (side == "bid") {
+    // if this price is within the bids orderbook
+    if (bids.find(price) != bids.end()) {
+      if (quantity ==
+          0) {  // means this bid deal is gone, remove it from order book
+        bids.erase(price);
+        return;
+      } else {
+        bids[price] = quantity;  // update to the latest quantity
+        return;
+      }
+    }
+
+    // this price not in the order book
+    if (quantity != 0) {
+      bids.insert({price, quantity});
+    }
+    if (bids.size() > ORDER_BOOK_SIZE) {
+      // remove the lowest price
+      auto minIt = std::min_element(bids.begin(), bids.end());
+      bids.erase(minIt);
+    }
+
+  } else if (side == "ask") {
+    if (asks.find(price) != asks.end()) {
+      if (quantity == 0) {
+        asks.erase(price);
+        return;
+      } else {
+        asks[price] = quantity;
+        return;
+      }
+    }
+
+    // this price not in the order book
+    if (quantity != 0) {
+      asks.insert({price, quantity});
+    }
+    if (asks.size() > ORDER_BOOK_SIZE) {
+      // remove the highest price
+      auto maxIt = std::max_element(asks.begin(), asks.end());
+      asks.erase(maxIt);
+    }
+  }
+}
+
+void updateOrderBook(json jsonMessage) {
+  long U = jsonMessage["U"];  // minId
+  long u = jsonMessage["u"];  // maxId
+
+  // process events only if U <= id + 1 <= u;
+  if (U <= lastUpdateId + 1 && lastUpdateId + 1 <= u) {
+    for (auto& bid : jsonMessage["b"]) {
+      manageOrderBook("bid", bid);
+    }
+
+    for (auto& ask : jsonMessage["a"]) {
+      manageOrderBook("ask", ask);
+    }
+    lastUpdateId = u;
+  } else if (u <= lastUpdateId) {  // one of the old messages, just do nothing
+    return;
+  } else {  // lastUpdateId < U, means our orderbook is outdated, we need to
+    initOrderBook();  // reset
+  }
 }
 
 // connection handler
@@ -78,8 +191,6 @@ void on_open(websocketpp::connection_hdl hdl, client* c) {
               << std::endl;
     return;
   }
-  //   std::string payload = "";
-  //   c->send(con, payload, websocketpp::frame::opcode::text);
 }
 
 void on_message(websocketpp::connection_hdl, client::message_ptr msg) {
@@ -91,8 +202,12 @@ void on_message(websocketpp::connection_hdl, client::message_ptr msg) {
   }
 
   try {
-    // auto json_message = json::parse(payload);
-    std::cout << "Parsed message: " << json::parse(payload) << std::endl;
+    auto json_message = json::parse(payload);
+    updateOrderBook(json_message);
+
+    printOrderBookTitle();
+    printBids();
+    printAsks();
   } catch (const std::exception& e) {
     std::cerr << "JSON parse error: " << e.what() << std::endl;
   }
@@ -122,17 +237,34 @@ context_ptr on_tls_init(const char* hostname, websocketpp::connection_hdl) {
   return ctx;
 }
 
-int main(int argc, char* argv[]) {
-  // get initial snapshot from binance
-  std::string snapshot = fetchSnapshot();
-  json jsonSnapshot = json::parse(snapshot);
-//   std::cout << jsonSnapshot << std::endl;
-  std::cout << "LastUpdatedId: " << jsonSnapshot["lastUpdateId"] << std::endl;
-  updateOrderBook(jsonSnapshot);
+std::string getCryptoPair() {
+  std::string pair;
+  std::cout << "What crypto pair are you interested in?: ";
+  std::getline(std::cin, pair);
 
+  return pair;
+}
+
+int main(int argc, char* argv[]) {
+  std::string pair = getCryptoPair();
+  std::cout << "Getting order book for " << pair << " ... " << std::endl;
+  // Sleep for 3 seconds for user to able to double check the correct symbol
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+
+  std::string upper = pair;
+  std::string lower = pair;
+  std::transform(upper.begin(), upper.end(), upper.begin(), ::toupper);
+  std::transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
+
+  BINANCE_SNAPSHOT_URL =
+      "https://api.binance.com/api/v3/depth?symbol=" + upper + "&limit=5";
+
+  if (!initOrderBook()) { // probably cryptopair is valid, can't get snapshot
+    return -1;
+  }
 
   client c;
-  std::string hostname = "stream.binance.com:9443/ws/bnbbtc@depth";
+  std::string hostname = "stream.binance.com:9443/ws/" + lower + "@depth";
   std::string uri = "wss://" + hostname;
 
   try {
